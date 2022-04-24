7c89d53e070dd53ab36c6c41fd51f27e3a75a8e0
Artsiom <Artyom.artyom.87@bk.ru>
2022-04-24 13:24:36 +0300

    change background in App.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,20 @@ import './App.css';
 import {Header} from './Components/Header/Header';
 import {Main} from './Components/Main/Main';
 // import {Footer} from './Components/Footer/Footer';
-import {BrowserRouter} from 'react-router-dom';
 import {StatePropsType} from './Redux/State';
 
 type AppType = {
-  state: StatePropsType
+    state: StatePropsType
 }
 
 export const App = (props: AppType) => {
 
     return (
-        <BrowserRouter>
-            <div className="wrapper">
-                <Header/>
-                <Main postsData={props.state.postsData} dialogsData={props.state.dialogsData} dialogMessages={props.state.dialogMessages}/>
-                {/*<Footer/>*/}
-            </div>
-        </BrowserRouter>
+        <div className="wrapper">
+            <Header/>
+            <Main postsData={props.state.postsData} dialogsData={props.state.dialogsData}
+                  dialogMessages={props.state.dialogMessages}/>
+            {/*<Footer/>*/}
+        </div>
     )
 }
